13dbabb74150a63473c1801022a2235230d4a96f
NedM <nmurphy@thinkmd.org>
2022-07-25 16:23:32 -0400

    Adding guard to show authorization flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 import appConfig from './config/app.config';
 
 @Module({
@@ -31,6 +32,7 @@ import appConfig from './config/app.config';
       }),
     }),
     CoffeeRatingModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
